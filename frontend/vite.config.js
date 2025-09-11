4063a3d98599ea1f7e80eb001dc4d53a66c6d4ca
z-fourteen <zgh1016@mail.usct.edu.cn>
2025-09-11 11:00:53 +0800

    feat: 添加feedback接口配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,11 @@ export default defineConfig({
   server: {
     // 配置代理
     proxy: {
+      '/feedback': 'http://127.0.0.1:8000',
       '/api': {
         // 这里是你的后端服务器地址
         target: 'http://127.0.0.1:8000', 
         changeOrigin: true,
-        // 可选：如果你的后端路由不以 /api 开头，可以重写路径
-        // rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
